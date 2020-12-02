4838b80fc43904ab3b43cd46d718e5982d07d696
Riccardo Russo <rr356@kent.ac.uk>
2020-12-02 15:18:21 +0000

    This is the Master Folder for our Project

---
+++ app/src/main/java/com/profileaccount/ProfileCreation.kt
@@ -0,0 +1,11 @@
+package com.profileaccount
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileCreation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_creation)
+    }
+}
